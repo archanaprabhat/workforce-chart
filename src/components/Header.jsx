d4c8684ac99b41e4f08459f3e3eca0bd8eb72e76
archanaprabhat <archanaprabhath2002@gmail.com>
2024-04-27 23:15:55 +0530

    new tab

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
  return (
     <div>
       <h1 className='text-5xl font-bold font-primary mx-8 mt-8'>React Employee Directory</h1>
-      <div className='text-gray-500 mx-8 mb-8'> Made with <FontAwesomeIcon icon={faHeart} style={{color: "#808080",}} /> by <a href="https://github.com/archanaprabhat" style={{color: "skyblue", textDecoration: "underline", textDecorationThickness: "2px",}}>Archana TK</a></div>
+      <div className='text-gray-500 mx-8 mb-8'> Made with <FontAwesomeIcon icon={faHeart} style={{color: "#808080",}} /> by <a href="https://github.com/archanaprabhat" target="_blank" rel="noopener noreferrer" style={{color: "skyblue", textDecoration: "underline", textDecorationThickness: "2px",}}>Archana TK</a></div>
       
     </div>
  )
